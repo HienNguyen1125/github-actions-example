f28e3315f5607177799c4b7c23930c6f4a1514ab
HienNguyen1125 <nguyenbaminhhienhust@gmail.com>
2024-04-19 10:19:30 +0700

    test
    test

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return "hello world";
 	}
 
 //	echo "# github-actions-example" >> README.md
@@ -19,7 +19,6 @@ public class GithubCicdActionsApplication {
 //	git add README.md
 //	git commit -m "first commit"
 //	git branch -M main
-//	git remote add origin https://github.com/Java-Techie-jt/github-actions-example.git
 //	git push -u origin main
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
